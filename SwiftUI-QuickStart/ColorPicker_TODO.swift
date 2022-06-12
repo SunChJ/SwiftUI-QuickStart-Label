af869867a3cc09430eda2e88b2db5110ab82d8c7
mrsunchaojie@163.com <350752900@qq.com>
2022-06-12 22:04:50 +0800

    chore: ColorPicker Purchase Course

---
+++ SwiftUI-QuickStart/ColorPicker_TODO.swift
@@ -0,0 +1,20 @@
+//
+//  ColorPicker_TODO.swift
+//  SwiftUI-QuickStart
+//
+//  Created by SamsonCJ on 2022/6/12.
+//
+
+import SwiftUI
+
+struct ColorPicker_TODO: View {
+    var body: some View {
+        Text("Purchased Course, TODO")
+    }
+}
+
+struct ColorPicker_TODO_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorPicker_TODO()
+    }
+}
