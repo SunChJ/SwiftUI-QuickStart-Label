e18071920268501402f803d6970470ae9390521e
mrsunchaojie@163.com <350752900@qq.com>
2022-06-12 20:00:37 +0800

    chore: LazyVGrid Purchased Course

---
+++ SwiftUI-QuickStart/LazyVGrid.swift
@@ -0,0 +1,20 @@
+//
+//  LazyVGrid.swift
+//  SwiftUI-QuickStart
+//
+//  Created by SamsonCJ on 2022/6/12.
+//
+
+import SwiftUI
+
+struct LazyVGrid_Introduction: View {
+    var body: some View {
+        Text("Purchased Course, TODO")
+    }
+}
+
+struct LazyVGrid_Previews: PreviewProvider {
+    static var previews: some View {
+        LazyVGrid_Introduction()
+    }
+}
