93a28fbc4ff61aa905949dc9576f0b5c7eacb75d
mrsunchaojie@163.com <350752900@qq.com>
2022-06-12 12:50:16 +0800

    Initial Commit

---
+++ SwiftUI-QuickStart/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUI-QuickStart
+//
+//  Created by SamsonCJ on 2022/6/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-QuickStart/SwiftUI_QuickStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_QuickStartApp.swift
+//  SwiftUI-QuickStart
+//
+//  Created by SamsonCJ on 2022/6/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_QuickStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
