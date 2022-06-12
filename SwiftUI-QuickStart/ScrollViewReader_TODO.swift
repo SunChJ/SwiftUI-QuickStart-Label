64cb2d01fb785ff127f6fa74f50c1074b23262b3
mrsunchaojie@163.com <350752900@qq.com>
2022-06-12 20:01:55 +0800

    chore: ScrollViewReader Purchased Course

---
+++ SwiftUI-QuickStart/ScrollViewReader_TODO.swift
@@ -0,0 +1,20 @@
+//
+//  ScrollViewReader_TODO.swift
+//  SwiftUI-QuickStart
+//
+//  Created by SamsonCJ on 2022/6/12.
+//
+
+import SwiftUI
+
+struct ScrollViewReader_TODO: View {
+    var body: some View {
+        Text("Purchased Course, TODO")
+    }
+}
+
+struct ScrollViewReader_TODO_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollViewReader_TODO()
+    }
+}
