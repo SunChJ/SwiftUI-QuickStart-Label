08f72d69c6b3aae9a37a8591ee669fd9515ada58
mrsunchaojie@163.com <350752900@qq.com>
2022-06-12 16:55:10 +0800

    chore: purchase course: LazyVStack & LazyHStack

---
+++ SwiftUI-QuickStart/LayzHStack.swift
@@ -0,0 +1,20 @@
+//
+//  LayzHStack.swift
+//  SwiftUI-QuickStart
+//
+//  Created by SamsonCJ on 2022/6/12.
+//
+
+import SwiftUI
+
+struct LayzHStack: View {
+    var body: some View {
+        Text("Purchased Course, TODO.")
+    }
+}
+
+struct LayzHStack_Previews: PreviewProvider {
+    static var previews: some View {
+        LayzHStack()
+    }
+}
---
+++ SwiftUI-QuickStart/LazyVStack.swift
@@ -0,0 +1,20 @@
+//
+//  LazyVStack.swift
+//  SwiftUI-QuickStart
+//
+//  Created by SamsonCJ on 2022/6/12.
+//
+
+import SwiftUI
+
+struct LazyVStack: View {
+    var body: some View {
+        Text("Purchased Course, TODO.")
+    }
+}
+
+struct LazyVStack_Previews: PreviewProvider {
+    static var previews: some View {
+        LazyVStack()
+    }
+}
