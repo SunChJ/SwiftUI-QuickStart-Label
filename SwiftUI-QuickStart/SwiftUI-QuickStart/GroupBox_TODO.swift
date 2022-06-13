79f8530fbd71d05a953fcdadd8e07b825500507b
mrsunchaojie@163.com <350752900@qq.com>
2022-06-13 19:56:55 +0800

    chore: GroupBox Purchase Course

---
+++ SwiftUI-QuickStart/SwiftUI-QuickStart/GroupBox_TODO.swift
@@ -0,0 +1,20 @@
+//
+//  GroupBox_TODO.swift
+//  SwiftUI-QuickStart
+//
+//  Created by SamsonCJ on 2022/6/13.
+//
+
+import SwiftUI
+
+struct GroupBox_TODO: View {
+    var body: some View {
+        Text("Hello, World!""Purchased Course, TODO")
+    }
+}
+
+struct GroupBox_Previews: PreviewProvider {
+    static var previews: some View {
+        GroupBox_TODO()
+    }
+}
