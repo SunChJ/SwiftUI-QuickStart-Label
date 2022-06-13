6e4aaed353b0133f68f589b294939039a566e2f3
mrsunchaojie@163.com <350752900@qq.com>
2022-06-13 19:58:32 +0800

    chore: Link Purchase Course

---
+++ SwiftUI-QuickStart/SwiftUI-QuickStart/Link_TODO.swift
@@ -0,0 +1,20 @@
+//
+//  Link_TODO.swift
+//  SwiftUI-QuickStart
+//
+//  Created by SamsonCJ on 2022/6/13.
+//
+
+import SwiftUI
+
+struct Link_TODO: View {
+    var body: some View {
+        Text("Purchased Course, TODO")
+    }
+}
+
+struct Link_TODO_Previews: PreviewProvider {
+    static var previews: some View {
+        Link_TODO()
+    }
+}
