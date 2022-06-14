38cd17c7ccceead0a7c7221763fc79f5f1e980e0
mrsunchaojie@163.com <350752900@qq.com>
2022-06-14 13:17:53 +0800

    chore: Addtional List Content  + Menu , Purchase Course

---
+++ SwiftUI-QuickStart/SwiftUI-QuickStart/Additional_List_Content_TODO.swift
@@ -0,0 +1,20 @@
+//
+//  Additional_List_Content_TODO.swift
+//  SwiftUI-QuickStart
+//
+//  Created by SamsonCJ on 2022/6/14.
+//
+
+import SwiftUI
+
+struct Additional_List_Content_TODO: View {
+    var body: some View {
+        Text("Purchased Course, TODO")
+    }
+}
+
+struct Additional_List_Content_TODO_Previews: PreviewProvider {
+    static var previews: some View {
+        Additional_List_Content_TODO()
+    }
+}
---
+++ SwiftUI-QuickStart/SwiftUI-QuickStart/Menu_TODO.swift
@@ -0,0 +1,20 @@
+//
+//  Menu_TODO.swift
+//  SwiftUI-QuickStart
+//
+//  Created by SamsonCJ on 2022/6/14.
+//
+
+import SwiftUI
+
+struct Menu_TODO: View {
+    var body: some View {
+        Text("Purchased Course, TODO")
+    }
+}
+
+struct Menu_TODO_Previews: PreviewProvider {
+    static var previews: some View {
+        Menu_TODO()
+    }
+}
